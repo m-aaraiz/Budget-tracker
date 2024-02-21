bbcf87bc4affc8fa4f224f110fbde1b38cb8baca
m-aaraiz <aaraizmo@gmail.com>
2024-02-20 19:16:25 -0700

    Initial setup of page including basic styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<h1>
+				$400<span>.00</span>
+			</h1>
+			<form>
+				<div className="basic">
+					<input type="text" placeholder={"+200 new transactions"} />
+					<input type="datetime-local" />
+				</div>
+				<div className="description">
+					<input type="text" placeholder={"Description"} />
+				</div>
+				<button>Add transaction</button>
+			</form>
+			<div className="transactions">
+				<div className="transaction">
+					<div className="left">
+						<div className="name">New TV</div>
+						<div className="description">
+							it was time for a New TV
+						</div>
+					</div>
+					<div className="right">
+						<div className="price">$500</div>
+						<div className="datetime">2024-02-20 2:45</div>
+					</div>
+				</div>
+				<div className="transaction">
+					<div className="left">
+						<div className="name">New TV</div>
+						<div className="description">
+							it was time for a New TV
+						</div>
+					</div>
+					<div className="right">
+						<div className="price">$500</div>
+						<div className="datetime">2024-02-20 2:45</div>
+					</div>
+				</div>
+				<div className="transaction">
+					<div className="left">
+						<div className="name">New TV</div>
+						<div className="description">
+							it was time for a New TV
+						</div>
+					</div>
+					<div className="right">
+						<div className="price">$500</div>
+						<div className="datetime">2024-02-20 2:45</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default App;
