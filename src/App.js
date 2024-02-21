0612d40f6df97d30afaea264c3946d0ab433dd59
m-aaraiz <aaraizmo@gmail.com>
2024-02-21 16:32:20 -0700

    Stlying to add colors and borders, including added some event handlers in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const [name, setName] = useState("");
+	const [datetime, setDatetime] = useState("");
+	const [description, setDescription] = useState("");
+
+	function addNewTransaction() {
+		
+	}
+
 	return (
 		<main>
 			<h1>
 				$400<span>.00</span>
 			</h1>
-			<form>
+			<form onSubmit={addNewTransaction}>
 				<div className="basic">
-					<input type="text" placeholder={"+200 new transactions"} />
-					<input type="datetime-local" />
+					<input
+						type="text"
+						value={name}
+						onChange={(ev) => setName(ev.target.value)}
+						placeholder={"+200 new transactions"}
+					/>
+					<input
+						value={datetime}
+						onChange={(ev) => setDatetime(ev.target.value)}
+						type="datetime-local"
+					/>
 				</div>
 				<div className="description">
-					<input type="text" placeholder={"Description"} />
+					<input
+						type="text"
+						value={description}
+						onChange={(ev) => setDescription(ev.target.value)}
+						placeholder={"Description"}
+					/>
 				</div>
 				<button>Add transaction</button>
 			</form>
@@ -25,31 +48,31 @@ function App() {
 						</div>
 					</div>
 					<div className="right">
-						<div className="price">$500</div>
+						<div className="price red">-$500</div>
 						<div className="datetime">2024-02-20 2:45</div>
 					</div>
 				</div>
 				<div className="transaction">
 					<div className="left">
-						<div className="name">New TV</div>
+						<div className="name">Paycheck</div>
 						<div className="description">
 							it was time for a New TV
 						</div>
 					</div>
 					<div className="right">
-						<div className="price">$500</div>
+						<div className="price green">+$500</div>
 						<div className="datetime">2024-02-20 2:45</div>
 					</div>
 				</div>
 				<div className="transaction">
 					<div className="left">
-						<div className="name">New TV</div>
+						<div className="name">Groceries</div>
 						<div className="description">
 							it was time for a New TV
 						</div>
 					</div>
 					<div className="right">
-						<div className="price">$500</div>
+						<div className="price red">-$100</div>
 						<div className="datetime">2024-02-20 2:45</div>
 					</div>
 				</div>
